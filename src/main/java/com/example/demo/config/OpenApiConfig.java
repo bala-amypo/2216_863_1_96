a688ddcc8c35133187d785a83be52b063a9cb95d
bala-amypo <bala@amypo.in>
2025-12-21 10:48:33 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,11 +1,9 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.List;
 
 @Configuration
@@ -14,15 +12,9 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // API Info
-                .info(new Info()
-                        .title("Leave Overlap - Team Capacity Analyzer API")
-                        .version("1.0")
-                        .description("API for managing employee leaves and team capacity analysis")
-                )
-                // Server configuration
+                // You need to change the port as per your server
                 .servers(List.of(
                         new Server().url("https://9388.pro604cr.amypo.ai/")
                 ));
-    }
+        }
 }
